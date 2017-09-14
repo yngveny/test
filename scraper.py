0820629fefd2e7907e710f6e25d9e1c8e7425bcc
yngveny <31400411+yngveny@users.noreply.github.com>
2017-09-14 14:04:57 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,7 +19,7 @@ html = scraperwiki.scrape("https://www.doffin.no/Notice?query=&PageNumber=1&Page
 
 root = lxml.html.fromstring(html)
 
-for el in root.cssselect("div.left-col a"):           
+for el in root.cssselect("div.left-col *"):           
     print lxml.html.tostring(el)
     #print el.text
     #scraperwiki.sqlite.save(unique_keys=['country'], data=lxml.html.tostring(el))
