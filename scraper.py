b225a6e5125fba993ca42c261745763ff7b29993
yngveny <31400411+yngveny@users.noreply.github.com>
2017-09-14 13:57:48 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,8 +19,8 @@ html = scraperwiki.scrape("https://www.doffin.no/Notice?query=&PageNumber=1&Page
 
 root = lxml.html.fromstring(html)
 
-for el in root.cssselect("div.left-col *"):           
-    print lxml.html.tostring(el)
-    #print el.text
+for el in root.cssselect("div.left-col a"):           
+    #print lxml.html.tostring(el)
+    print el.text
 
 print "And done!"
