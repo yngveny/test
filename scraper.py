7083c9903d3e097566c0abe3451ff6e2748e7158
yngveny <31400411+yngveny@users.noreply.github.com>
2017-09-14 14:24:12 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -19,7 +19,7 @@ html = scraperwiki.scrape("https://www.doffin.no/Notice?query=&PageNumber=1&Page
 
 root = lxml.html.fromstring(html)
 
-for el in root.cssselect("div.notice-search-item-header *"):           
+for el in root.cssselect("div.right-col *"):           
     print lxml.html.tostring(el, pretty_print=True)
     #print el.text
     #scraperwiki.sqlite.save(unique_keys=[el.text], data=lxml.html.tostring(el))
