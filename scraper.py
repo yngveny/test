2e4d63e2ebb29f28c9cd55f695c7b32a8f33a7e8
yngveny <31400411+yngveny@users.noreply.github.com>
2017-09-14 12:39:50 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -24,3 +24,7 @@
 # called "data".
 
 print "Hello, coding in the cloud!"
+
+import scraperwiki           
+html = scraperwiki.scrape("http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm")
+print html
